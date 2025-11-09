f46445ef51bc5a5b40f68bff61d7f06584bb362e
Mostafiz002 <mo.mahin4@gmail.com>
2025-11-10 03:21:51 +0600

    initialized firebase and created context to use it

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,87 @@
+import React, { createContext, useEffect, useState } from "react";
+import app from "../Firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+const auth = getAuth(app);
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [forgotEmail, setForgotEmail] = useState("");
+  const googleProvider = new GoogleAuthProvider();
+
+  //create user
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //create user with photo and name
+  const updateUser = (updatedData) => {
+    return updateProfile(auth.currentUser, updatedData);
+  };
+
+  // logout
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  // login
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // reset password
+  const resetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  // google signin
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const authData = {
+    user,
+    setUser,
+    loading,
+    createUser,
+    updateUser,
+    logOut,
+    signIn,
+    googleSignIn,
+    setForgotEmail,
+    forgotEmail,
+    resetPassword,
+  };
+
+  return <AuthContext value={authData}>{children}</AuthContext>;
+};
+
+export default AuthProvider;
