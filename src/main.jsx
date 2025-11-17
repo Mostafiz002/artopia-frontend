ea69726ab9cca5580e51e259849a9f5845ddd41e
Mostafiz002 <mo.mahin4@gmail.com>
2025-11-17 22:20:25 +0600

    added smooth scrolling with lenis

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,21 @@ import { RouterProvider } from "react-router";
 import router from "./routes/route.jsx";
 import AuthProvider from "./provider/AuthProvider.jsx";
 import { Toaster } from "react-hot-toast";
+import ReactLenis from "lenis/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-      <Toaster
-        position="top-center"
-        reverseOrder={true}
-        toastOptions={{
-          duration: 3000,
-        }}
-      />
-    </AuthProvider>
+    <ReactLenis root options={{ smoothWheel: true, lerp: 0.1 }}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <Toaster
+          position="top-center"
+          reverseOrder={true}
+          toastOptions={{
+            duration: 3000,
+          }}
+        />
+      </AuthProvider>
+    </ReactLenis>
   </StrictMode>
 );
